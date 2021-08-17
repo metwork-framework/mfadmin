b1f957ccd69776c4c8b764612d47cf0fdeee5187
Fabien MARTY <fabien.marty@gmail.com>
2021-08-17 10:52:38 +0200

    Update _force_grafana_admin_password.sh

--- adm/_force_grafana_admin_password.sh
+++ adm/_force_grafana_admin_password.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "${MFADMIN_GRAFANA_ADMIN_PASSWORD}" |/opt/metwork-mfadmin-1.1/opt/metrics/opt/grafana/bin/grafana-cli --config "${MFMODULE_RUNTIME_HOME}/tmp/config_auto/grafana.ini" --homepath "${MFMODULE_HOME}/opt/metrics/opt/grafana" admin reset-admin-password --password-from-stdin
+echo "${MFADMIN_GRAFANA_ADMIN_PASSWORD}" |"${MFADMIN_HOME}/opt/metrics/opt/grafana/bin/grafana-cli" --config "${MFMODULE_RUNTIME_HOME}/tmp/config_auto/grafana.ini" --homepath "${MFMODULE_HOME}/opt/metrics/opt/grafana" admin reset-admin-password --password-from-stdin
