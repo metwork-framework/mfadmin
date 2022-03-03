b456c3c696493e6172befe3634902bfaf35e9466
Fabien MARTY <fabien.marty@meteo.fr>
2022-03-03 20:28:50 +0000

    build: fix

--- layers/layer3_ldap/0600_ldap_code/start.sh
+++ layers/layer3_ldap/0600_ldap_code/start.sh
@@ -2,4 +2,4 @@
 
 cd ${MFADMIN_HOME}/opt/ldap || exit 1
 layer_load ldap@mfadmin
-exec uvicorn repaire.api.infra.main:app --reload --port=8000 --workers=1 --log-level=debug
+exec uvicorn repaire.api.infra.main:app --reload --port=$1 --workers=1 --log-level=debug
