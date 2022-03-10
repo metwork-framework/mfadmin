f6b999bab1e6346b678ccbf02a80b59be81aa2a9
Fabien MARTY <fabien.marty@meteo.fr>
2022-03-10 10:40:30 +0000

    build: ajout d'une variable de conf

--- layers/layer3_ldap/0600_ldap_code/start.sh
+++ layers/layer3_ldap/0600_ldap_code/start.sh
@@ -7,5 +7,6 @@ export REPAIRE_LDAP_BIND_WHO="${MFADMIN_LDAP_BIND_WHO}"
 export REPAIRE_LDAP_BIND_PASS="${MFADMIN_LDAP_BIND_PASS}"
 export REPAIRE_LDAP_URL="${MFADMIN_LDAP_URL}"
 export REPAIRE_LDAP_BASE="${MFADMIN_LDAP_BASE}"
+export REPAIRE_LDAP_STARTTLS="${MFADMIN_LDAP_STARTTLS}"
 export REPAIRE_COOKIE_NAME="mfadmin_token"
 exec layer_wrapper --layers=ldap@mfadmin -- uvicorn repaire.api.infra.main:app --port=$1 --workers=1 
