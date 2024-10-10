d70f2ec803e4d4ff82cca573d73da4871a930eb8
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2024-10-10 10:30:35 +0200

    fix: replace deprecated utcnow() by now(datetime.UTC) (#312)

--- layers/layer3_ldap/0600_ldap_code/repaire/api/app/token.py
+++ layers/layer3_ldap/0600_ldap_code/repaire/api/app/token.py
@@ -23,8 +23,9 @@ def make_jwt(
         "uid_number": user.uid_number,
         "email": user.email,
         "display_name": user.display_name,
-        "iat": datetime.datetime.utcnow(),
-        "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=delta),
+        "iat": datetime.datetime.now(datetime.UTC),
+        "exp": datetime.datetime.now(datetime.UTC) + \
+                datetime.timedelta(seconds=delta),
     }
     if len(limit_to_vhosts) > 0:
         payload["limit_to_vhosts"] = limit_to_vhosts
