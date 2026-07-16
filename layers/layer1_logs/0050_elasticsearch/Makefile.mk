825bc6453f701190719fc19609fc8eceda440e2b
Manu <emmanuel.graille@meteo.Fr>
2026-07-16 06:38:59 +0000

    feat: bump elasticsearch and kibana from 8.18.2 to 8.19.18

--- layers/layer1_logs/0050_elasticsearch/Makefile.mk
+++ layers/layer1_logs/0050_elasticsearch/Makefile.mk
@@ -2,10 +2,10 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=elasticsearch
-export VERSION=8.18.2
+export VERSION=8.19.18
 export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=60561954bc33816636f1a30a7c73b840
+export CHECKSUM=dc3aef85c211a5a628527104da748b80
 DESCRIPTION=\
 Elasticsearch is a distributed, RESTful search and analytics engine
 WEBSITE=https://www.elastic.co/products/elasticsearch
